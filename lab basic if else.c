afa0372a7b01bf35bc4c3d4a29ae95aaa0db125c
Mohammad Nazmul Hossain <57767915+Naz365@users.noreply.github.com>
2020-02-11 14:27:54 +0600

    Add files via upload

---
+++ lab basic if else.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("enter the value of two number : ");
+    scanf ("%d %d",&num1,&num2);
+    if(num1>num2)
+{
+    printf("%d is greater number ",num1);
+}
+if (num2>num1)
+{
+    printf("%d is greater number",num2);
+
+}
+else {
+    printf("this value is incorrect");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
