04d97e47678c8c49b03a9fe168b8743f70154829
Mohammad Nazmul Hossain <57767915+Naz365@users.noreply.github.com>
2020-02-21 23:18:08 +0600

    Add files via upload

---
+++ lab find a maximum number.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int num1,num2,num3;
+    printf ("Enter the number : ");
+    scanf("%d %d %d",&num1,&num2,&num3);
+
+    if(num1>num2)
+    {
+        if (num1>num3)
+        {
+            printf("%d is the maximum number",num1);
+
+        }
+        else
+        {
+            printf("%d is the maximum number",num3);
+        }
+    }
+    else
+    {
+        if (num2>num3)
+        {
+            printf("%d is the maximum number",num2);
+        }
+        else
+        {
+            printf("%d is the mazimum number",num3);
+
+        }
+    }
+return 0;
+
+
+
+
+
+}
