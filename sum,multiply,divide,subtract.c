f83b87b9cc6a4897c49f6a8c4d8c8976471827e4
Mohammad Nazmul Hossain <57767915+Naz365@users.noreply.github.com>
2020-03-11 14:59:17 +0600

    Update and rename class.c to sum,multiply,divide,subtract.c

--- sum,multiply,divide,subtract.c
+++ sum,multiply,divide,subtract.c
@@ -5,15 +5,25 @@ int main()
     int num1,num2,sum,multiply,divide,subtract;
 
     printf("Enter two number :");
+    
     scanf("%d %d",&num1,&num2);
+    
     sum=num1+num2;
+    
     multiply=num1*num2;
+    
     divide=num1/num2;
+    
     subtract=num1-num2;
+    
     printf("sum=%d\n",sum);
+    
     printf("multiply=%d\n",multiply);
+    
     printf("divide=%d\n",divide);
+    
     printf("subtrac=%d\n",subtract);
+    
     return 0;
 
 }
