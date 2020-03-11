f1ff335c4b947f78fd58b010c6c9a0396288adc0
Mohammad Nazmul Hossain <57767915+Naz365@users.noreply.github.com>
2020-03-11 05:26:04 -0700

    Add files via upload

---
+++ lab find power.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+int number,result=1,power,finally;
+int i=1;
+printf("enter a number : ");
+scanf("%d %d", &number,&power);
+for (i=1;i<=power;i++)
+{
+ result*=number;
+
+
+}
+
+printf("%d",result);
+
+scanf("%d 1/rerult",&finally);
+printf("The negative value is :",finally);
+
+return 0;
+
+}
